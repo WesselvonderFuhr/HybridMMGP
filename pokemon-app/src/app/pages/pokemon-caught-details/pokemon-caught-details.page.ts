e027c880a54fb25bfaf64b302429ba607d21fe03
WesselvonderFuhr <38494974+WesselvonderFuhr@users.noreply.github.com>
2021-03-31 17:22:13 +0200

    storage

--- pokemon-app/src/app/pages/pokemon-caught-details/pokemon-caught-details.page.ts
+++ pokemon-app/src/app/pages/pokemon-caught-details/pokemon-caught-details.page.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Directive, OnInit } from '@angular/core';
 import {PokemonService} from '../../services/pokemon.service';
 import {Pokemon} from '../../models/pokemon';
 import {ActivatedRoute} from '@angular/router';
 import { Plugins } from '@capacitor/core';
+import { PokemonStorage } from 'src/app/models/pokemon-storage';
 
 const { Storage } = Plugins;
 
@@ -13,45 +14,57 @@ const { Storage } = Plugins;
   styleUrls: ['./pokemon-caught-details.page.scss'],
 })
 export class PokemonCaughtDetailsPage implements OnInit {
- 
-
-
-  pokemon: Pokemon;
+  name: String;
+  description: String;
+  pokemon: PokemonStorage ;
 
   constructor(private route: ActivatedRoute,
-    private pokemonService: PokemonService) { }
-
+    private pokemonService: PokemonService) {
+      this.pokemon = new PokemonStorage(null,0,0,'','');
+   
+     }
+    
   ngOnInit(): void {
-    // this.dataService.getPokemon(id).subscribe(pokemon => this.pokemon = pokemon);
     const id = +this.route.snapshot.paramMap.get('id');
-    this.pokemonService.getPokemon(id).subscribe(pokemon => this.pokemon = pokemon);
-   this.clear();
+    this.setPokemonObject(id,this.getPokemonWithid,this);
   }
+
+  SaveInfo(){
+    this.pokemon.name = this.name;
+    this.pokemon.description = this.description;
+    Storage.set({
+      key: this.pokemon.id.toString(),
+      value: JSON.stringify({
+        pokemon_id:  this.pokemon.pokemon.id,
+        pokemon_name:  this.pokemon.pokemon.name,
+        name: this.pokemon.name,
+        description: this.pokemon.description
+      })
+    });
+   
+  }
+  async setPokemonObject(key,_callback,classThis) {
+    let ret = await Storage.get({ key: key});
+    let json = JSON.parse(ret.value);
+    console.log(json);
+    this.pokemon.id = parseInt(key);
+    this.pokemon.name = json.name;
+    this.pokemon.description = json.description;
+    this.pokemon.pokemon_id = json.pokemon_id;
+    this.name = this.pokemon.name;
+    this.description = this.pokemon.description;
+    _callback(json.pokemon_id,classThis)
+  }
+  getPokemonWithid(id,classThis){
+    classThis.pokemonService.getPokemon(id).subscribe(pokemon => classThis.pokemon.pokemon = pokemon);
+  }
+
+
+
+  //storage
   async clear() {
     await Storage.clear();
   }
-  async keys() {
-    const { keys } = await Storage.keys();
-    console.log('Got keys: ', keys);
-  }
-// JSON "set" example
-async setObject(keyname) {
-  await Storage.set({
-    key: keyname,
-    value: JSON.stringify({
-      id: 1,
-      name: 'Max'
-    })
-  });
-}
-
-// JSON "get" example
-async getObject() {
-  const ret = await Storage.get({ key: 'user' });
-  const user = JSON.parse(ret.value);
-}
-
-
 
 }
 
